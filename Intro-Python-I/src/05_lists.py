284f6523250f6b17fc0c45cb96938c1615014e12
harjoth <harjoth.khara@gmail.com>
2020-02-12 11:07:47 -0800

    lists, added extra condition problem

--- Intro-Python-I/src/05_lists.py
+++ Intro-Python-I/src/05_lists.py
@@ -8,22 +8,34 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)  # adds a new value to the end of the list
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+# could also concatenate the lists using + and save to a new variable.
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+x.remove(8)  # removes first occurance of value
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(len(x)-1, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for value in x:
+    print(value * 1000)
+
+# only print values in list if condition is met
+list1 = [value for value in x if value > 9]
+print(list1)
