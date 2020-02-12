fd0778b00b17eb81f7fcd83906eaf309dce1c671
harjoth <harjoth.khara@gmail.com>
2020-02-12 15:53:47 -0800

    list comprehensions - for loop logic in list brackets, creating new list based on another list. syntactical construct

--- Intro-Python-I/src/08_comprehensions.py
+++ Intro-Python-I/src/08_comprehensions.py
@@ -2,22 +2,26 @@
 List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
-the list should be populated. 
+the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
+
+Comprehensions in Python. Comprehensions in Python provide us with a short and concise way to construct new sequences (such as lists, set, dictionary etc.) using sequences which have been already defined.
+
+List comprehensions are used for creating new lists from other iterables. As list comprehensions returns lists, they consist of brackets containing the expression, which is executed for each element along with the for loop to iterate over each element. ... Here, square brackets signifies that the output is a list
 """
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [i for i in range(1, 6)]
 
-print (y)
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
-
-y = []
+# creates a new list containing the cubes of all values from range(10)
+y = [i**3 for i in range(10)]
 
 print(y)
 
@@ -26,7 +30,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [words.upper() for words in a]
 
 print(y)
 
@@ -35,7 +39,8 @@ print(y)
 
 x = input("Enter comma-separated numbers: ").split(',')
 
-# What do you need between the square brackets to make it work?
-y = []
+# What do you need between the square brackets to make it work? used int() build in python method
+# elements in x are strings, need to convert each to int before using any math operation
+y = [elements for elements in x if int(elements) % 2 == 0]
 
 print(y)
