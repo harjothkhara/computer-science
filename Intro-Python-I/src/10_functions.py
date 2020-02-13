244f43538d50f65bbfed99192aed55f6fb0ef4be
harjoth <harjoth.khara@gmail.com>
2020-02-12 19:39:17 -0800

    function definition - even-odd check

--- Intro-Python-I/src/10_functions.py
+++ Intro-Python-I/src/10_functions.py
@@ -3,10 +3,22 @@
 # YOUR CODE HERE
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+num = input("Enter a number: ")  # returns a string
+num = int(num)  # convert to an int for math computation
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+# defining function
+
+
+def is_even(number):
+    if number % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
+
+
+# calling function
+is_even(num)
