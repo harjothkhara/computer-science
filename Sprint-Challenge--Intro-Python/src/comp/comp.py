5474d24884874ef48ab14a9064bd10f1b05da39b
harjoth <harjoth.khara@gmail.com>
2020-02-21 18:18:10 -0800

    list comprehension complete

--- Sprint-Challenge--Intro-Python/src/comp/comp.py
+++ Sprint-Challenge--Intro-Python/src/comp/comp.py
@@ -15,6 +15,7 @@ class Human:
         return f"<Human: {self.name}, {self.age}>"
 
 
+# list called 'humans' than instantiates an instance of the Human class
 humans = [
     Human("Alice", 29),
     Human("Bob", 32),
@@ -28,50 +29,59 @@ humans = [
     Human("David", 31),
 ]
 
+# [  FORMATTING            COLLECTION                   CONDITION  ]
+
+# a[start:stop]  # items start through stop-1
+# a[start:]      # items start through the rest of the array
+# a[:stop]       # items from the beginning through stop-1
+# a[:]           # a copy of the whole array
+
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
-print("Starts with D:")
-a = []
+#print("Starts with D:")
+a = [human.name for human in humans if human.name[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
-print("Ends with e:")
-b = []
+#print("Ends with e:")
+b = [human.name for human in humans if human.name[-1] == 'e']
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
-print("Starts between C and G, inclusive:")
-c = []
+# print("Starts between C and G, inclusive:")
+c = [human.name for human in humans if human.name[0:1] in ['C', 'D', 'E', 'F', 'G']]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
-print("Ages plus 10:")
-d = []
+#print("Ages plus 10:")
+d = [human.age + 10 for human in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
-print("Name hyphen age:")
-e = []
+#print("Name hyphen age:")
+e = [f"{human.name}-{human.age}" for human in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
-print("Names and ages between 27 and 32:")
-f = []
+#print("Names and ages between 27 and 32:")
+f = [(human.name, human.age)
+     for human in humans if 27 <= human.age <= 32]
 print(f)
 
-# Write a list comprehension that creates a list of new Humans like the old
+# Write a list comprehension that creates a list of new Humans [like] the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
-print("All names uppercase:")
-g = []
+# not a tuple :-) changing the format of the class instance - needs to be same format as original list!!!
+#print("All names uppercase:")
+g = [Human(human.name.upper(), human.age + 5) for human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
-print("Square root of ages:")
-h = []
+# print("Square root of ages:")
+h = [math.sqrt(human.age) for human in humans]
 print(h)
