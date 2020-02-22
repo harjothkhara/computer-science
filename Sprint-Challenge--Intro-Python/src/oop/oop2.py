8b1b20b61e34f22941c7b4de5f808ab795d9f61a
harjoth <harjoth.khara@gmail.com>
2020-02-21 17:02:46 -0800

    oop2 - attributes and method to base class, super, method overiding, practicing polymorphism

--- Sprint-Challenge--Intro-Python/src/oop/oop2.py
+++ Sprint-Challenge--Intro-Python/src/oop/oop2.py
@@ -3,12 +3,19 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
-class GroundVehicle():
-    def __init__(self, num_wheels):
+
+# base class
+class GroundVehicle:
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    # methods
+    def drive(self):
+        return f"vroooom, i have {self.num_wheels} wheels"
 
+
+g = GroundVehicle().drive()
+# print(g)
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -19,6 +26,25 @@ class GroundVehicle():
 
 # TODO
 
+
+class Motorcycle(GroundVehicle):
+    # constructor
+    def __init__(self):
+        # inherited attributes from super class
+        # set number of wheels to 2 by passing it to the constructor of Motorcycle superclass
+        super().__init__(num_wheels=2)
+        # any specific attributes for Motorcyle class
+
+    # method (overidding)
+    def drive(self):
+        super().drive()
+        return f"BRAAAP!! i have {self.num_wheels} wheels"
+
+
+m = Motorcycle().drive()
+# print(m)
+
+# list called 'vehicles' that is called instances of the both classes
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -30,3 +56,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for vehicle in vehicles:
+    print(vehicle.drive())
