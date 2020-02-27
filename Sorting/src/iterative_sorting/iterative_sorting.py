27f4ea0d61435046ca11520cb04b9799a431a32d
harjoth <harjoth.khara@gmail.com>
2020-02-27 13:52:25 -0800

    bubble sort complete after adding nested loop

--- Sorting/src/iterative_sorting/iterative_sorting.py
+++ Sorting/src/iterative_sorting/iterative_sorting.py
@@ -14,14 +14,13 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 
+#           ✅     ✅    ✅   ✅   ✅ 
+# # index    0      1     2     3    4
+#     arr = [2,     3,    5,    6,   8]
+#     #      |      |     |     |    |- 'sorted bubbled number'
+#    i=3    j=0   j+1
 
-# index    0   1   2   3   4
-    arr = [2,  5,  3,  6,  8]
-    #      |   |   |   |   |- 'sorted bubbled number'
-    #     i=0 i=1 i=2 i=3
-
-i = 3
-swap = False
+#    swap = False
 
 
 def bubble_sort(arr):
@@ -29,17 +28,22 @@ def bubble_sort(arr):
     # get index and loop through array -  no need to check end 'bubbled number'
     for i in range(0, len(arr)-1):
         # print(i)  # 0, 1, 2, 3,
-        #     - Compare each element to its neighbor
-        if arr[i] > arr[i+1]:
-            #     - If elements in wrong position (relative to each other, swap them)
-            swap = True
-            arr[i], arr[i+1] = arr[i+1], arr[i]
+        # iterate through list j times for every i so we catch every unsorted item
+        # -i representes the index of the last moving unsorted item in list.
+        for j in range(0, len(arr)-i-1):
+            #     - Compare each element to its neighbor
+            if arr[j] > arr[j+1]:
+                #     - If elements in wrong position (relative to each other, swap them)
+                swap = True
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 # 2. If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
-        else:
-            swap = False
+            else:
+                swap = False
     print(arr)
 
-#[2, 5, 3, 6, 8] <-- our list is not ordered yet (need to iterate through each list int and compare more then once). need to do a nested for loop
+# [2, 5, 3, 6, 8] <-- our list is not ordered yet (need to iterate through each list int and compare more then once). need to do a nested for loop
+
+# [2, 3, 5, 6, 8] < --- our list is now ordered after adding the nested loop
 
 
 bubble_sort(arr=[5,  2,  6,  3,  8])
