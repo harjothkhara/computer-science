50683c8d849fc3fa618fd28828517a3d300b4914
harjoth <harjoth.khara@gmail.com>
2020-03-05 14:36:19 -0800

    exploring dictionary key/values

--- Algorithms/recipe_batches/recipe_batches.py
+++ Algorithms/recipe_batches/recipe_batches.py
@@ -2,13 +2,26 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    counts = []
+
+    # items() returns all dict keys with values
+    for name, value in recipe.items():
+        print(f"recipe items, {name}, {value}")
+        if name in ingredients:
+            print(f"name is also in ingredient", {name})
+    for j in ingredients.values():
+        print(f"ingredient values, {j}")
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+recipe_batches({'milk': 100, 'butter': 50, 'cheese': 10},
+               {'milk': 198, 'butter': 52, 'cheese': 10})
+
+# if __name__ == '__main__':
+#     # Change the entries of these dictionaries to test
+#     # your implementation with different inputs
+#     recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+#     ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+#     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+#         batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
