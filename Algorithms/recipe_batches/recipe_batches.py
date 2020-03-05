ac4cf7e2a2c107ac0154aa295a7cb541b932b826
harjoth <harjoth.khara@gmail.com>
2020-03-05 15:08:52 -0800

    planned out via comments and in process of executing

--- Algorithms/recipe_batches/recipe_batches.py
+++ Algorithms/recipe_batches/recipe_batches.py
@@ -5,18 +5,30 @@ import math
 
 def recipe_batches(recipe, ingredients):
     counts = []
-
-    # items() returns all dict keys with values
+    # access key/value in recipe dict
     for name, value in recipe.items():
         print(f"recipe items, {name}, {value}")
+        # check how much ingredients can be used according to recipe specs
         if name in ingredients:
             print(f"name is also in ingredient", {name})
-    for j in ingredients.values():
-        print(f"ingredient values, {j}")
+            # add complete ingredient batch to counter
+            counts.append(math.floor(ingredients[name] / value))
+        else:
+            counts.append(0)
+    # check how many times each ingredient can be used
+    print(counts)
+
+    # see which ingredient can be used more than others
+
+    # check counts array for any ingredient usable in more cycles than the other ingredients, if found, set equal to max batch
+
+    # return max batch
 
 
 recipe_batches({'milk': 100, 'butter': 50, 'cheese': 10},
                {'milk': 198, 'butter': 52, 'cheese': 10})
+
+# Dict Notes - items() returns all dict keys with values
 
 # if __name__ == '__main__':
 #     # Change the entries of these dictionaries to test
