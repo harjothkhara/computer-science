93af9f871f786fb6253085adece9cec843aaa403
harjoth <harjoth.khara@gmail.com>
2020-03-06 17:27:02 -0800

    finally workable solution to recusive word count

--- Sprint-Challenge--Algorithms/recursive_count_th/count_th.py
+++ Sprint-Challenge--Algorithms/recursive_count_th/count_th.py
@@ -3,8 +3,18 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    count = 0
+    # basecase - checking for "th" occurrences
+    if word.startswith("th"):  # returns boolean
+        count = 1
+
+    # cases for recursive call
+    if len(word) > 2:
+        # removing first letter as we make our checks
+        # returning count if "th" was found
+        return count_th(word[1:len(word)] + count)
+    else:
+        return count
