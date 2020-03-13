5344db52ee2f23d1cb8e2b22fa7ac9f2351ce1bd
harjoth <harjoth.khara@gmail.com>
2020-03-13 16:41:20 -0700

    stack whiteboard added, tests pass

--- Data-Structures/queue_and_stack/dll_stack.py
+++ Data-Structures/queue_and_stack/dll_stack.py
@@ -11,11 +11,13 @@ class Stack:
 
 # stack is last in first out
 
+    # adding a new node to the list (end of stack) <->H
     def push(self, value):
-        self.storage.add_to_head(value)
+        self.storage.add_to_head(value)  # could also use add_to_tail
 
+    # remove last node in from list and return its value (last item in stack is the fist one out)
     def pop(self):
-        return self.storage.remove_from_head()
+        return self.storage.remove_from_head()  # could also use remove_from_tail
 
     def len(self):
         return self.storage.length
