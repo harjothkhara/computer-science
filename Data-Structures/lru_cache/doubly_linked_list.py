640ae28d68acbb7692abf82d9390d2f160b30ddd
harjoth <harjoth.khara@gmail.com>
2020-03-15 16:37:30 -0700

    DLL - move_to_front, move_to_end, delete

--- Data-Structures/lru_cache/doubly_linked_list.py
+++ Data-Structures/lru_cache/doubly_linked_list.py
@@ -131,19 +131,46 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        # already at head
+        if node is self.head:
+            return
+        # otherwide delete node at current location
+        self.delete(node)
+        # move node to head
+        self.add_to_head(node.value)
 
     """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
 
-    def move_to_end(self, node):
-        pass
+    def move_to_end(self, node):  # LRU position
+        # already at tail
+        if node is self.tail:
+            return
+        # otherwise delete node at current position
+        self.delete(node)
+        # move node to tail
+        self.add_to_tail(node)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail. """
 
     def delete(self, node):
-        pass
+        self.length -= 1
+        # only 1 item in list
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        # item at head
+        elif self.head == node:
+            self.head = self.head.next
+            node.delete()
+        # item at tail
+        elif self.tail == node:
+            self.tail = self.tail.prev
+            node.delete()
+        # item in middle
+        else:
+            node.delete()
 
     """Returns the highest value currently in the list"""
 
