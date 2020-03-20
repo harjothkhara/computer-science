c7e347562940fe30afb4729707d54404ba2714ee
harjoth <harjoth.khara@gmail.com>
2020-03-20 16:17:40 -0700

    Data Structures Sprint - getting started!

---
+++ Sprint-Challenge--Data-Structures-Python/ring_buffer/ring_buffer.py
@@ -0,0 +1,34 @@
+from doubly_linked_list import DoublyLinkedList
+
+
+class RingBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
+
+    def append(self, item):  # adds elements to the buffer
+        pass
+
+    def get(self):
+        # returns all the elements in the buffer in a list in their given order
+        # should not return any None values in the list even if they are present in the ring buffer
+        # Note:  This is the only [] allowed
+        list_buffer_contents = []
+
+        # TODO: Your code here
+
+        return list_buffer_contents
+
+# ----------------Stretch Goal-------------------
+
+
+class ArrayRingBuffer:
+    def __init__(self, capacity):
+        pass
+
+    def append(self, item):
+        pass
+
+    def get(self):
+        pass
