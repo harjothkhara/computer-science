755ff6a4875735ffd4d7303db926fb2613de5e12
harjoth <harjoth.khara@gmail.com>
2020-04-17 17:47:30 -0700

    hashtables ex2

--- Sprint-Challenge--Hash-BC/hashtables/ex2/ex2.py
+++ Sprint-Challenge--Hash-BC/hashtables/ex2/ex2.py
@@ -18,6 +18,32 @@ def reconstruct_trip(tickets, length):
 
     """
     YOUR CODE HERE
+    since we need all the tickets, insert all the tickets to a HT
+    key:value
+    source: destination
+    if source is None, first trip, append the destination to results
+    while destination is not None, append the destination to the results
     """
+    flight_order = []
+    # since we need all the tickets, insert all the tickets to a HT
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    if hash_table_retrieve(hashtable, "NONE"):
+        flight_order.append(hash_table_retrieve(hashtable, "NONE"))
+
+    for i in range(0, length - 2):
+        flight_order.append(hash_table_retrieve(
+            hashtable, flight_order[-1:][0]))
+
+    return flight_order
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+result = reconstruct_trip(tickets, 3)
+print(result)
