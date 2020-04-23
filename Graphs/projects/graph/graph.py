07f2f03d33d338036a0530db678d225123386205
harjoth <harjoth.khara@gmail.com>
2020-04-22 19:02:24 -0700

    graph project - working on dft recursive plan

--- Graphs/projects/graph/graph.py
+++ Graphs/projects/graph/graph.py
@@ -3,9 +3,11 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,33 +15,83 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        # inserting a key:value into vertices dict. key is the vertex number and the value is a set()
+        self.vertices[vertex_id] = set()
+        #  key: value
+        # { 5: set(), 8: set(), 9: set()...}
 
-    def add_edge(self, v1, v2):
+    def add_edge(self, v1, v2):  # adding edges to our set
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise ValueError("vertex does not exist")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        else:
+            raise ValueError("vertex does not exist")
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
+        Traversal = visiting each vertex in the graph
         """
-        pass  # TODO
+        # Create a queue - FIFO
+        q = Queue()
+        # Enqueue the starting vertex
+        q.enqueue(starting_vertex)
+        # Create a set to store visited vertices
+        visited = set()
+        # While the queue is not empty...
+        while q.size() > 0:
+            # Dequeue the first vertex
+            v = q.dequeue()
+            # Check if it's been visited
+            # If it hasn't been visited...
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Enqueue all of its neighbors
+                for neighbor in self.get_neighbors(v):
+                    q.enqueue(neighbor)
+
+# queue = [2,3]
+# visited = {1}
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
+        Traversal = visiting each vertex in the graph
         """
-        pass  # TODO
+        # create a stack - LIFO
+        s = Stack()
+        # Push the starting vertex
+        s.push(starting_vertex)
+        # Create a set to stored visited vertices
+        visited = set()
+        # While the stack is not empty
+        while s.size() > 0:
+            # Pop the last vertex in
+            v = s.pop()
+            # Check if it's been visited
+            # If is hasn't been visited
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Push all of its neighbors
+                for neighbor in self.get_neighbors(v):
+                    s.push(neighbor)
 
     def dft_recursive(self, starting_vertex):
         """
@@ -48,7 +100,19 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        s = Stack()
+        s.push(starting_vertex)
+        visited = set()
+        while s.size() > 0:
+            # pop last vertex on the stack
+            v = s.pop()
+        # Check if the node has been visited
+        # If not...
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)  # add it to our visited set
+                # Call dft_recursive on each neighbor
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -56,7 +120,23 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        # Create a queue - FIFO
+        # Enqueue A PATH TO the starting vertex
+        # Create a set to store visited vertices
+        # While the queue is not empty...
+        # Dequeue the first PATH
+        # GRAB THE VERTEX FROM THE END OF THE PATH
+        # Check if it's been visited
+        # If it hasn't been visited...
+        # Mark it as visited
+        # CHECK IF IT'S THE TARGET
+        # IF SO, RETURN THE PATH
+        # Enqueue A PATH TO all of its neighbors
+        # MAKE A COPY OF THE PATH
+        # ENQUEUE THE COPY
+# target = 6
+# q = [ [1] ]
+# visited = {}
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -75,6 +155,7 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
@@ -101,7 +182,7 @@ if __name__ == '__main__':
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
-    print(graph.vertices)
+    print(F"Graph: {graph.vertices}")
 
     '''
     Valid BFT paths:
@@ -118,7 +199,8 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    graph.bft(1)
+    print("BFT")
+    graph.bft(1)  # starting vertex
 
     '''
     Valid DFT paths:
@@ -127,19 +209,20 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
-    graph.dft(1)
-    graph.dft_recursive(1)
+    print("DFT")
+    graph.dft(1)  # starting vertex
+    # graph.dft_recursive(1)
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    print(graph.bfs(1, 6))
+    # print(graph.bfs(1, 6))
 
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
+    # print(graph.dfs(1, 6))
+    # print(graph.dfs_recursive(1, 6))
--- Graphs/projects/graph/util.py
+++ Graphs/projects/graph/util.py
@@ -1,28 +1,38 @@
 
 # Note: This Queue class is sub-optimal. Why?
-class Queue():
+# dequeue is popping an item from the front of the array, which we means we need to shift everything over before we find and pop item - o(n). if we know our n's are going to be big then we could refactor to use a LL or python built-in deck (double ended queue). since our n's are small this is ok.
+
+
+class Queue():  # FIFO
     def __init__(self):
-        self.queue = []
+        self.queue = []  # efficient if we declare size of an array upfront
+
     def enqueue(self, value):
         self.queue.append(value)
+
     def dequeue(self):
         if self.size() > 0:
+            # from front of an array we need to shift everything over so 0(n)
             return self.queue.pop(0)
         else:
             return None
+
     def size(self):
         return len(self.queue)
 
-class Stack():
+
+class Stack():  # LIFO
     def __init__(self):
-        self.stack = []
+        self.stack = []  # efficient if we declare size of an array upfront
+
     def push(self, value):
         self.stack.append(value)
+
     def pop(self):
         if self.size() > 0:
-            return self.stack.pop()
+            return self.stack.pop()  # from back of an array no shifts 0(1)
         else:
             return None
+
     def size(self):
         return len(self.stack)
-
