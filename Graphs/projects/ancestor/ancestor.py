5a1e435458d71f702029700c8b89b7faf874fe55
harjoth <harjoth.khara@gmail.com>
2020-04-26 20:23:29 -0700

    graphs day2 project - ancestors problem (UPER step1 - whiteboarding)

--- Graphs/projects/ancestor/ancestor.py
+++ Graphs/projects/ancestor/ancestor.py
@@ -1,3 +1,10 @@
 
+# Since we want to find the farthest parent, we'll be using DFS
+# We only need to return the parent, not a path
+# We only want to move towards parents
+
+# (parent, child)
+
+
 def earliest_ancestor(ancestors, starting_node):
     pass
