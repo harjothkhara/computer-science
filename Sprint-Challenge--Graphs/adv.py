ea7dc14006618735f0ec1c75a40d5911990dcf49
harjoth <harjoth.khara@gmail.com>
2020-05-05 11:50:46 -0700

    starting computer architecture project

--- Sprint-Challenge--Graphs/adv.py
+++ Sprint-Challenge--Graphs/adv.py
@@ -13,10 +13,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -42,25 +42,32 @@ def dft_walk(room, visited=None):
     explored_path = []
     # add the current room's ID to visited
     visited.add(room.id)
+    # print(f"visited:{visited}")
     # for each potential exit we can choose in this room ...
     for this_exit in room.get_exits():
+        # print(f"exit:{this_exit}")
         # move the player to a room in direction of exit
         new_room = room.get_room_in_direction(this_exit)
+        # print(f"new-room:{new_room}")
         # if the new room hasn't been visited yet...
         if new_room.id not in visited:
-            # explore the new room, update visited, and check to see if its a valid path (not a dead-end) - recursively call
+            # explore the new room, update visited, and check to see if its a valid path (not a dead-end) - recursive call in chosen room.
             valid_path = dft_walk(new_room, visited)
             # if it is a valid path we move deep into the maze
+            # print(f"valid-path:{valid_path}")
             if valid_path:
-                # update our current path to include valid path
+                # update our current path to include valid path (move deeper into maze)
                 current_path = [this_exit] + valid_path + \
                     [flip_directions[this_exit]]
+                # print(f"current-path:{current_path}")
             # else, not a valid path...
             else:
                 # update our current path and exclude invalid path (backtrack)
                 current_path = [this_exit, flip_directions[this_exit]]
+                # print(f"current-path-else:{current_path}")
             # whether the path was valid or not, add the current path to our explored path and reassign variable
             explored_path = explored_path + current_path
+            # print(f"explored-path:{explored_path}")
     # return explored path (after all the loops and conditionals are complete)
     return explored_path
 
@@ -87,7 +94,7 @@ else:
 
 print("")
 # print traversal path
-print(f"traversal-path: {traversal_path}")
+# print(f"traversal-path: {traversal_path}")
 print("")
 
 #######
