0560dc648abc2159f934d44c0f26e0e1940db156
harjoth <harjoth.khara@gmail.com>
2020-07-24 17:31:54 -0700

    sorting a stack using another stack problem

--- Coding-Challenges/sorting-a-stack.js
+++ Coding-Challenges/sorting-a-stack.js
@@ -25,5 +25,36 @@ class Stack {
 }
 
 function sortStack(input) {
-  //
+  // create our output stack
+  const output = new Stack();
+
+  // create our temp var = null
+  let temp = null;
+  // while loop over our input stack until input stack is empty
+  while (!input.isEmpty()) {
+    // pop off the top input element and put into a temp var
+    temp = input.pop();
+    // while - compare it to the peaked element of the output stack, if temp is less OR if it's empty
+    while (!output.isEmpty() && temp < output.peek()) {
+      // do the pop-push :tada
+      input.push(output.pop());
+    }
+    // push temp onto output
+    output.push(temp);
+  }
+  return output;
 }
+
+const s = new Stack();
+s.push(4);
+s.push(10);
+s.push(8);
+s.push(5);
+s.push(1);
+s.push(6);
+
+const sortedStack = sortStack(s);
+
+sortedStack.printContents();
+
+// Technical Coaching:  https://www.youtube.com/watch?v=W14h8_4ziFk
