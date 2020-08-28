1a401054e060c124d0312e0ffb20aefa99964e8f
harjoth <harjoth.khara@gmail.com>
2020-08-27 17:29:09 -0700

    leetcode: shuffle the array

---
+++ Coding-Challenges/Leetcode/shuffle_the_array.js
@@ -0,0 +1,17 @@
+// shuffle the array
+// https://leetcode.com/problems/shuffle-the-array/
+
+var shuffle = function(nums, n) {
+  // match corresponding x and y neighbors together inside of new array
+      const newArr = []
+      for(let i=0; i<n; i++){
+             newArr.push(nums[i])
+             newArr.push(nums[i+n])
+      }
+      return newArr
+  };
+  
+  const arr = [2,5,1,3,4,7]
+  shuffle(arr,3)
+
+  
