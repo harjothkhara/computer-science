060bad796f7bf044e0f313f150462ac90251caf3
harjoth <harjoth.khara@gmail.com>
2020-09-22 19:37:03 -0700

    optimized + cleaner valid parantheses solution

--- Coding-Challenges/Leetcode/valid_parentheses.js
+++ Coding-Challenges/Leetcode/valid_parentheses.js
@@ -38,3 +38,58 @@
       // in the end if the stack is empty, then we have balanced brackets
       return true
   };
+
+  // alternate - better runtime solution
+  var isValid = function(s) {
+    // use a stack data structure for the correct order
+    
+    // loop through character string
+    // if open parentheses type then add to the stack (deal with them later to find a matching closing parentheses)
+        
+    // if closing parentheses then we check the the top of the stack for a corresponding open parentheses 
+            // if a match is found we pop the open parentheses from the stack
+
+    // empty stack then our input string is valid, we have all matching parentheses 
+    
+    
+    let stack = []
+    // edge case - an odd number of char cannot be valid
+    if(s.length%2 !== 0 ) return false
+    
+    for(let i=0; i<s.length; i++){
+        // open parenthesis
+        if(s[i] === '(' || s[i] === '{' || s[i] === '['){
+            stack.push(s[i])
+        }
+        // closed parenthesis
+        else if(s[i] === ')') {
+            if(stack[stack.length-1] === '('){ // is it at the top of the stack
+                stack.pop()
+            }   
+            else{
+                return false
+            }    
+        }
+        else if(s[i] === '}'){
+            if(stack[stack.length-1] === '{'){ // is it at the top of the stack
+                stack.pop()
+            }   
+            else{
+                return false
+            }    
+        }
+        else if(s[i] === ']'){
+            if(stack[stack.length-1] === '['){ // is it at the top of the stack
+                stack.pop()
+            }   
+            else{
+                return false
+            }    
+        }
+    }
+    // return true if our stack is empty, we've matched all the open parenetheses and popped them
+    return stack.length === 0
+    
+};
+// O(n) time complexity b/c we move through the string one char at a time push and popping operations on a stack O(1) time.
+// O(n) as we push all opening brackets onto the stack, worst case we push all brackets on the stack e.g (((((((
