e3c6f297cfbe1e0c75a1e75cedff51589bddc70d
harjoth <harjoth.khara@gmail.com>
2020-10-28 19:20:54 -0700

    HR: new years chaos, second day practice w/ big o

--- Coding-Challenges/HackerRank/Arrays/NewYearsChaos.js
+++ Coding-Challenges/HackerRank/Arrays/NewYearsChaos.js
@@ -39,3 +39,55 @@ function minimumBribes(q) {
   }
   console.log(bribes)
 }
+
+// same solution as above but with notes and Big 0 (second day practice!)
+function minimumBribes(q) {
+  // [2,1,5,4,3]    
+// 1. no bribes at i (filter to see if there was any bribes)
+// 2. bribes (going down the queue and sorting it in its rightful order)
+  // person at i bribed the person in front (i-1)
+          // swapping (arr, i, i-1)
+          // bribes++
+  // person at i bribes 2 people in front (i-2)
+          // swapping (arr, i, i-1)
+         // swapping (arr, i-1, i-2) 
+         // bribes+=2
+  // person at i did more bribes (too chaotic) 
+      // print("too chaotic")
+  let bribes = 0
+  // temp = 8
+             // |    
+  // [1 2 5 3 7 6 4 8]
+  for(let i=q.length-1; i>=0; i--){
+      if(q[i] !== i+1){ // filter to see if a bribe took place
+          // check i-1 position
+          if(q[i-1] === i+1){ // send q[i] back to its origin spot (sort)
+              // swapping (arr, i, i-1)
+              let temp = q[i-1]
+              q[i-1] = q[i]
+              q[i] = temp
+              // increment swap count
+              bribes++
+            // check i-2 position  
+          } else if(q[i-2] === i+1){ // send q[i] back to its origin spot (sort)
+              // swapping (arr, i-1, i-2)
+              let temp = q[i-2]
+              q[i-2] = q[i-1]
+              q[i-1] = temp
+          // swapping (arr, i, i-1)
+              temp = q[i-1]
+              q[i-1] = q[i]
+              q[i] = temp
+              // increment swap count 
+              bribes+=2
+          } else {
+              bribes = "Too chaotic"
+              break
+          }
+      }
+  }
+  console.log(bribes)
+}
+
+// time - O(n)
+// space 0(1)
