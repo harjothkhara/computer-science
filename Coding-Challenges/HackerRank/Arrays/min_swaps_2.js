8fd052e63b37924572564a2fc3638bf8ba749876
harjoth <harjoth.khara@gmail.com>
2020-10-30 18:14:57 -0700

    HR optimized passing min array swap w/ diagram. thanks YT!

--- Coding-Challenges/HackerRank/Arrays/min_swaps_2.js
+++ Coding-Challenges/HackerRank/Arrays/min_swaps_2.js
@@ -36,3 +36,73 @@ function minimumSwaps(arr) {
 }
 
 // need to find a better solution because won't work with large numbers (1<=n<=10**5)
+function minimumSwaps(arr) {
+  // pre-processing
+  const shift = Math.min(...arr) // lowest num in the array
+  let swaps = 0
+  let visited = [Array(arr.length).fill(false)] // [false, false, false, false...] 
+                  // [] also works
+
+  // iterate through the array
+          // check if num in current index is in the correct spot, or, if num has already been visited. if                either then return and go to next num in the loop
+          
+          // x = i
+          // loopLength = 0
+
+          // else, iterate through current nums intended places until we reach a current index that has                been visited (cycle)
+              // mark current index as visited
+              // go to index where num should be (x= arr[x] - shift) index 1 -> index 2
+              
+          //count the cycle (length-1) to get the swap count. increment swap count
+
+  // return the min swap count
+
+  for(let i=0; i<arr.length; i++){ // O(n)
+          // 1. in the correct spot or has already been visited move to next iteration round
+          if(arr[i] === shift+i || visited[i]) continue
+          
+          // 2. not in correct spot, find a cycle!
+          // in wrong spot, go searching for arr[i]'s intended spot until we find a cycle
+          let x = i
+          // keep track of loop count
+          let loopLength = 0
+          while(!visited[x]){ // find a cycle. - O(logn) at most half way through
+              visited[x] = true //
+              // lets go to the intended place of arr[x]
+              x = arr[x] - shift
+              loopLength++
+          }
+          // found a cycle, lets count it and add to swap count
+          swaps += loopLength-1 
+      }
+  return swaps
+}  // O(n) * O(logn)
+
+// Youtube solution - https://www.youtube.com/watch?v=f7IIW0HVUcQ
+const minimumSwaps = (arr) => {
+  // pre-processing
+  const shift = Math.min(...arr)
+  let swaps = 0
+  const visited = [...Array(arr.length)].map(() => false)
+  // iterate through each value in the array
+  arr.forEach((val, i) => {
+    // if the current value is in the correct spot in the array 
+    // or, if that spot in the array has already been visited, do nothing 
+    if (val - shift === i || visited[i]) return
+    // else, use a graph to find all the sub cycles
+    let loopLength = 0
+    let x = i
+    while (!visited[x]) {
+      visited[x] = true
+      x = arr[x] - shift
+      loopLength++
+    }
+    // add the length of the cycle to get # of swaps
+    swaps += loopLength - 1
+  })
+  // once we've finished iterating return the swap count
+  return swaps
+}
+// O(nlogn)
+let arr = [0,2,3,4,1,6,5]
+ minimumSwaps(arr)
